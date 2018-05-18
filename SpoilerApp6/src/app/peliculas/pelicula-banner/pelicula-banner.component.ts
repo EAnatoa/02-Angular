e9cc5b1fa5968b3bd520c725214644a2aca7c869
EAnatoa <stefy_nf92@hotmail.com>
2018-05-18 17:50:55 -0500

    pelicula

--- SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+
 
 @Component({
   selector: 'app-pelicula-banner',
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
+/*
 export class PeliculaBannerComponent implements OnInit {
 
   urlImagen = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg';
@@ -14,11 +16,39 @@ export class PeliculaBannerComponent implements OnInit {
   esEstreno = true;
   textoEstreno: string;
   claseEstreno: string;
+*/
+export class PeliculaBannerComponent implements
+  OnInit, OnChanges {
+
+  @Input() urlImagen: string;
+  @Input() descripcionImagen: string;
+  @Input() nombrePelicula: string;
+  @Input() descripcionPelicula: string;
+  @Input() esEstreno: boolean;
+
+
+  @Output() dioClickEnEstado: EventEmitter<boolean> = new EventEmitter();
+
+
+  textoEstreno: string;
+  claseEstreno: string;
 
   constructor() {
   }
 
   ngOnInit() {
+    console.log("Inicio y seteo color",this.nombrePelicula);
+    this.setearColoryClase();
+  }
+
+  ngOnChanges(propiedadesActualizadas){
+    console.log('Algo',propiedadesActualizadas);
+    if (propiedadesActualizadas.esEstreno){
+      this.setearColoryClase();
+    }
+  }
+
+  setearColoryClase(){
     if (this.esEstreno) {
       this.textoEstreno = 'Estreno';
       this.claseEstreno = 'sa-color-estado-rosado';
@@ -28,4 +58,7 @@ export class PeliculaBannerComponent implements OnInit {
     }
   }
 
+  hizoClickEnEstado(){
+    this.dioClickEnEstado.emit(true);
+  }
 }
