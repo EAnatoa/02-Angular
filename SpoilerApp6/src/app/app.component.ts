c579db66c5b51e130c2cb52e9c81a7e51fe8948a
EAnatoa <stefy_nf92@hotmail.com>
2018-05-21 15:12:34 -0500

    peliculas secundarias

--- SpoilerApp6/src/app/app.component.ts
+++ SpoilerApp6/src/app/app.component.ts
@@ -9,7 +9,33 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   mostrarHola = true;
-  arregloNumeros = [1, 2, 3, 4, 5, 6];
+  arregloPeliculasSecundarias = [
+    {
+      nombre: 'Black Panther',
+      descripcion: '',
+      esEstreno: true,
+      urlImagen: 'https://i.blogs.es/c7f5ce/blackpanther/450_1000.jpg'
+    },
+    {
+      nombre: 'Maze Runner',
+      descripcion: '',
+      esEstreno: true,
+      urlImagen: 'https://images.penguinrandomhouse.com/cover/9781524714451'
+    },
+    {
+      nombre: 'Star Wars',
+      descripcion: '',
+      esEstreno: true,
+      urlImagen: 'https://liberal.com.mx/wp-content/uploads/2017/12/eu_sws-8_last-jedi_hero_v9_r_ddc759e5.jpeg'
+    },
+    {
+      nombre: 'Liga de la justicia',
+      descripcion: '',
+      esEstreno: true,
+      urlImagen: 'https://www.cronista.com/__export/1511228969706/sites/diarioelcronista/img/2017/11/21/cc211117w023f11.jpg_258117318.jpg'
+    },
+  ];
+
 
   arregloPeliculas = [
     {
--- SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.spec.ts
+++ SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.spec.ts
@@ -1,20 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeliculaBannerComponent } from './pelicula-banner.component';
+import {DeberPeliculasComponent} from "../../deber-peliculas/deber-peliculas.component";
 
-describe('PeliculaBannerComponent', () => {
-  let component: PeliculaBannerComponent;
-  let fixture: ComponentFixture<PeliculaBannerComponent>;
+describe('DeberPeliculasComponent', () => {
+  let component: DeberPeliculasComponent;
+  let fixture: ComponentFixture<DeberPeliculasComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PeliculaBannerComponent ]
+      declarations: [ DeberPeliculasComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PeliculaBannerComponent);
+    fixture = TestBed.createComponent(DeberPeliculasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
